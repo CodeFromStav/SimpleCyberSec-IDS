6baab6c4ebf7e03d9ca9105c032c46d6d81d6569
Stavros <stav.triantis@gmail.com>
2023-04-23 17:00:31 -0700

    Added two alert logs which both output Event Type: dhcp, one for 'src port' & one for 'src ip'

--- securityIDS.py
+++ securityIDS.py
@@ -2,13 +2,15 @@ import json
 import pandas as pd
 
 # Read the JSON log file
-with open('/var/log/suricata/eve.json', 'r') as f:
-    log_entries = [json.loads(line) for line in f]
+with open('/var/log/suricata/eve.json', 'r') as file_iter:
+    log_entries = [json.loads(line) for line in file_iter]
 
 # Load the data into a pandas DataFrame
 df = pd.DataFrame(log_entries)
 
 # Filter, analyze, or aggregate the data as needed
 # For example, count the number of alerts by severity
-alert_counts = df[df['event_type'] == 'alert']['severity'].value_counts()
-print(alert_counts)
+# print(df.columns)
+alert_counts, alert_counts2 = df[df['event_type'] == 'dhcp']['src_port'].value_counts(), df[df['event_type'] == 'dhcp']['src_ip'].value_counts()
+
+print("Alert Counts:\n" + str(alert_counts) + str(alert_counts2))
