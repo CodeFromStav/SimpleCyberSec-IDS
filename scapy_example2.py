ab3eaf9d9550d11e1ffd8e74901992148e361a3a
Stavros <stav.triantis@gmail.com>
2023-04-28 12:54:21 -0700

    Changed code to capture packets and store data in a list, then create a DataFrame from the list. Saves content as JSON or CSV.

--- scapy_example2.py
+++ scapy_example2.py
@@ -2,19 +2,20 @@ from scapy.all import sniff
 from scapy.layers.inet import IP
 import time
 from protocols import protocol_references #my protocol dictionary
+import pandas as pd
 
 # Configuration
 INTERFACE = "wlp59s0"  # Change this to the interface you want to monitor, e.g., wlan0, en0
-PACKET_COUNT = 50  # Number of packets to capture
+PACKET_COUNT = 25  # Number of packets to capture
 OUTPUT_FILE = "network_activity.log"
 
 def get_protocol_description(protocol_number):
     return protocol_references.get(protocol_number, "Unknown Protocol")
 
-
 # Packet handler
 def packet_handler(packet):
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
     src_ip = "N/A"
     dst_ip = "N/A"
     protocol = "N/A"
@@ -26,9 +27,30 @@ def packet_handler(packet):
 
     protocol_description = get_protocol_description(protocol)        
 
-    log_entry = f"{timestamp} | {src_ip} -> {dst_ip} | Protocol: {protocol} - {protocol_description}\n"
-    with open(OUTPUT_FILE, "a") as f:
-        f.write(log_entry)
+    new_row = {
+        "Timestamp": timestamp,
+        "Source IP": src_ip,
+        "Destination IP": dst_ip,
+        "Protocol": protocol,
+        "Protocol Description": protocol_description
+    }
 
-# Capture packets
-sniff(iface=INTERFACE, count=PACKET_COUNT, prn=packet_handler)
+    return new_row
+
+# Capture packets and store the data in a list
+packet_list = []
+for packet in sniff(iface=INTERFACE, count=PACKET_COUNT):
+    packet_info = packet_handler(packet)
+    packet_list.append(packet_info)
+
+# Create a DataFrame from the list of dictionaries
+packet_data = pd.DataFrame.from_records(packet_list)
+
+#save to JSON
+packet_data.to_json("network_activity.json", orient="records", lines=True)
+
+# #save to CSV
+# packet_data.to_csv(OUTPUT_FILE, index=False)
+
+
+
